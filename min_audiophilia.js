c25e96dea16bc0c81471440cfef5acd9aad773ae
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2014-02-12 23:27:37 +0000

    min_audiophilia

---
+++ min_audiophilia.js
@@ -0,0 +1,228 @@
+(function(){
+var init = function() {
+	if (DEBUG) console.log('script start to execute: '+(Date.now()-window.startScript)+' ms');
+
+	window.requestAnimationFrame || (window.requestAnimationFrame = 
+		window.webkitRequestAnimationFrame || 
+		window.mozRequestAnimationFrame    || 
+		window.oRequestAnimationFrame      || 
+		window.msRequestAnimationFrame     || 
+		function(callback, element) {
+			return window.setTimeout(function() {
+				callback(Date.now());
+			}, 1000 / 60);
+		}
+	);
+
+	var createTexture = function(gl, buf, unit) {
+		gl.activeTexture( gl.TEXTURE0+(unit||0) );
+		var tex = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, tex);
+		gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+		if (buf instanceof Float32Array) {
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, buf.width, buf.height, 0, gl.RGBA, gl.FLOAT, buf);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+		} else {
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, buf.width, buf.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, buf);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+			gl.generateMipmap(gl.TEXTURE_2D);
+		}
+		return tex;
+	};
+	var updateTexture = function(gl, tex, buf, unit) {
+		gl.activeTexture( gl.TEXTURE0+(unit||0) );
+		gl.bindTexture(gl.TEXTURE_2D, tex);
+		if (buf instanceof Float32Array) {
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, buf.width, buf.height, 0, gl.RGBA, gl.FLOAT, buf);
+		} else {
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, buf.width, buf.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, buf);
+		}
+	};
+	var createBuffer = function(gl) {
+		var buf = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+		var arr = new Float32Array([
+			-1,-1, 0,
+			 1,-1, 0,
+			 1, 1, 0,
+			-1,-1, 0,
+			 1, 1, 0,
+			-1, 1, 0
+		]);
+		gl.bufferData(gl.ARRAY_BUFFER, arr, gl.STATIC_DRAW);
+		return buf;
+	};
+	var createShader = function(gl, source, type) {
+		var s = source;
+		if (typeof source === 'string') {
+			s = gl.createShader(type);
+			gl.shaderSource(s, source);
+			gl.compileShader(s);
+			if (!gl.getShaderParameter(s, gl.COMPILE_STATUS)) {
+				throw new Error(gl.getShaderInfoLog(s));
+			}
+		}
+		return s;
+	};
+	var createProgram = function(gl, vert, frag) {
+		var t0 = Date.now();
+		var p = gl.createProgram();
+		var vs = createShader(gl, vert, gl.VERTEX_SHADER);
+		var fs = createShader(gl, frag, gl.FRAGMENT_SHADER);
+		gl.attachShader(p, vs);
+		gl.attachShader(p, fs);
+		gl.linkProgram(p);
+		if (DEBUG) console.log('Create program: '+(Date.now()-t0)+' ms');
+		return p;
+	};
+	var getUniform = function(gl, p, name) {
+		return gl.getUniformLocation(p, name);
+	};
+	var u4fv = function(gl, p, name, v) {
+		gl.uniform4fv(getUniform(gl, p, name), v);
+	};
+	var u3fv = function(gl, p, name, v) {
+		gl.uniform3fv(getUniform(gl, p, name), v);
+	};
+	var u3f = function(gl, p, name, x,y,z) {
+		gl.uniform3f(getUniform(gl, p, name), x,y,z);
+	};
+	var u1f = function(gl, p, name, x) {
+		gl.uniform1f(getUniform(gl, p, name), x);
+	};
+	var u1i = function(gl, p, name, x) {
+		gl.uniform1i(getUniform(gl, p, name), x);
+	};
+
+	var vec3 = function(x,y,z) {
+		var v = new Float32Array(3);
+		x = x || 0;
+		v[0] = x;
+		v[1] = y || x;
+		v[2] = z || x;
+		return v;
+	};
+
+	Loader.get(shaderURLs, function() {
+		var t1 = Date.now();
+		var t0 = Date.now();
+		var buf = createBuffer(gl);
+		var rTex = createTexture(gl, randomTex, 0);
+		if (DEBUG) console.log('Set up WebGL: '+(Date.now()-t0)+' ms');
+		var iResolution = vec3(glc.width, glc.height, 1.0);
+
+		var resize = function() {
+			glc.width = window.innerWidth;
+			glc.height = window.innerHeight;
+			iResolution[0] = glc.width;
+			iResolution[1] = glc.height;
+			gl.viewport(0,0, glc.width, glc.height);
+			u3fv(gl, p, 'iResolution', iResolution);
+		};
+
+		var p;
+		var shaders = arguments;
+		var currentShader = 0;
+		var rtVert = 'precision highp float;attribute vec3 position;void main() {gl_Position = vec4(position, 1.0);}';
+		var rtShader = createShader(gl, rtVert, gl.VERTEX_SHADER);
+		var setShader = function(idx) {
+			currentShader = idx;
+			p = shaders[currentShader];
+			if (typeof p === 'string') {
+				p = shaders[currentShader] = createProgram(gl, rtShader, p);
+			}
+			gl.useProgram(p);
+			u3fv(gl, p, 'iResolution', iResolution);
+			u1i(gl, p, 'iChannel0', 0);
+			u1i(gl, p, 'iChannel1', 1);
+			var pos = gl.getAttribLocation(p, 'position');
+			gl.enableVertexAttribArray(pos);
+			gl.vertexAttribPointer(pos, 3, gl.FLOAT, false, 0, 0);
+		};
+		setShader(currentShader);
+
+
+		var t = 0;
+		var targetRot = 0;
+		var targetOpen = 0;
+		var mouse = new Float32Array(4);
+		var iRot = 0;
+		var iOpen = 0;
+
+		var tDown = 0;
+
+		var down = false;
+
+		glc.onmousedown = function(ev) {
+			mouse[2] = ev.layerX;
+			mouse[3] = this.offsetHeight-ev.layerY;
+			targetRot -= 0.25*Math.PI;
+			down = true;
+			tDown = t;
+			ev.preventDefault();
+		};
+		glc.onmouseup = function(ev) {
+			mouse[2] = -1;
+			mouse[3] = -1;
+			targetOpen = targetOpen ? 0 : 1;
+			down = false;
+			ev.preventDefault();
+		};
+		glc.onmousemove = function(ev) {
+			mouse[0] = ev.layerX;
+			mouse[1] = this.offsetHeight-ev.layerY;
+		};
+		window.onresize = resize;
+
+		var blurred = false;
+		window.onblur = function() {
+			blurred = true;
+		};
+		window.onfocus = function() {
+			blurred = false;
+		};
+		if (DEBUG) console.log('WebGL setup total: '+(Date.now()-t1)+' ms'); 
+
+		var cameraPos = new Float32Array(4); // xyz, roll angle
+		var cameraPosV = new Float32Array(4); // xyz, roll angle
+		var cameraTarget = new Float32Array(4); // xyz, zoom
+		var cameraTargetV = new Float32Array(4); // xyz, zoom
+		var cx0, cy0, cz0;
+		var x0,y0,z0,i,j;
+		var dt = 16/1000;
+		var cdir = vec3(0.0);
+		var target = 7;
+		var tick = function() {
+			if (window.startScript) {
+				if (window.performance && performance.timing && performance.timing.navigationStart) {
+					console.log('navigationStart to first frame: '+(Date.now()-performance.timing.navigationStart)+' ms');
+				}
+				console.log('script start to first frame: '+(Date.now()-window.startScript)+' ms');
+				window.startScript = 0;
+			}
+			t += 16;
+			iRot += (targetRot - iRot) * 0.1;
+			u1f(gl, p, 'iGlobalTime', t/1000);
+			u1f(gl, p, 'iRot', iRot);
+			gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+			gl.drawArrays(gl.TRIANGLES, 0, 6);
+			requestAnimationFrame(tick, glc);
+		};
+		resize();
+		tick();
+		document.body.appendChild(glc);
+	});
+};
+var ticker = function() {
+	if (window.gl) init();
+	else setTimeout(ticker, 0);
+};
+ticker();
+})();
+

298fbdab221aa3a20303da8ef60b6bc7b5b5a031
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2014-07-02 01:20:57 +0100

    remove portfolio

--- zardoz.js
+++ zardoz.js
@@ -514,7 +514,7 @@ ticker();
 			SC.initialize({
 			    client_id: "7edc86ef9d085d9b071f1c1b7199a205"
 			});
-			window.scplayer = new SCPlayer("/tracks/77262295", document.getElementById('music'));			
+			window.scplayer = new SCPlayer("/tracks/99386315", document.getElementById('music'));			
 		} else if (ticks < 100) {
 			ticks++;
 			setTimeout(sctick, 100);

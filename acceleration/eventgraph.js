6273c50eb18b0e6ffe1d992b1b0da1f70c505ca8
Ilmari Heikkinen <ilmari@fhtr.net>
2017-07-05 11:53:56 +0800

    event graph thought, style for object

---
+++ acceleration/eventgraph.js
@@ -0,0 +1,9 @@
+/*
+ * EventGraph
+ *
+ * Visual node-based dataflow language.
+ *
+ * Functions connected into an execution graph.
+ * Each function is a rounded rectangle with inputs, outputs
+ * and next-steps.
+ */

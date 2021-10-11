96aa5fd78f399de00a83746ef404361e291146d8
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2021-10-11 22:51:20 +0800

    cornfield: optimize wind arrow

--- cornfield_full/js/weathertick.js
+++ cornfield_full/js/weathertick.js
@@ -10,6 +10,7 @@ var targetCityIndex = 0;
 
 var cityChangeDuration = 3000;
 var weatherUpdateTriggered = false;
+var windArrow = document.getElementById('wind-direction-arrow').transform.baseVal.getItem(0);
 
 var setWeather = function() {
 	weatherTimer += 16;
@@ -67,7 +68,7 @@ var setWeather = function() {
 	shaderMat.uniforms.ufSunPosition.value = 2*timeOfDay / 86400; // 0 = left = 6:00, 0.5 = up = 12:00, 1 = right = 18:00, 1.5 = down = 24:00
 	// shaderMat.uniforms.ufSunPosition.value = (Date.now() / 3000) % 2;
 
-	if (shaderMat.uniforms.ufSunPosition.value > 0.1 && shaderMat.uniforms.ufSunPosition.value < 0.3) {
+	if (shaderMat.uniforms.ufSunPosition.value > 0.1 && shaderMat.uniforms.ufSunPosition.value < 0.5) {
 		document.body.classList.add('daytime');
 	} else {
 		document.body.classList.remove('daytime');		
@@ -85,7 +86,7 @@ var setWeather = function() {
 	// Add ground effect noise to wind for the rain & grass.
 	rainShaderMat.uniforms.ufWindDirection.value = Math.PI + windDirection / 180 * Math.PI + wdOff;
 	rainShaderMat.uniforms.ufWindStrength.value = windStrength / 20 * 0.4 + wsOff;
-	document.getElementById('wind-direction-arrow').transform.baseVal.getItem(0).setRotate(windDirection, 7.5, 7.5);
+	windArrow.setRotate(windDirection, 7.5, 7.5);
 
 	cornShaderMat.uniforms.ufImpulse.value = 0.00; // Math.max(-1, -0.5*Math.cos(Math.PI*((2*timeOfDay / 86400) % 2)) - 0.5);
 };

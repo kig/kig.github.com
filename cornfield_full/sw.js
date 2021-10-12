617fb3885d136ceee3fba2eb39267b569a7e969f
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2021-10-12 10:24:40 +0800

    cornfield: add external-site cache to service worker

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,10 +1,11 @@
-const CACHE = 'cornfield-cache-v2';
+const APP_CACHE = 'cornfield-cache-v2';
+const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;
 
 self.addEventListener('install', (e) => {
     self.skipWaiting();
     e.waitUntil(
-        caches.open(CACHE).then((cache) => cache.addAll([
+        caches.open(APP_CACHE).then((cache) => cache.addAll([
             './',
             './index.html',
             './images/weather_icon.jpg',
@@ -25,13 +26,16 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('fetch', async (evt) => {
     if (/^https:\/\/api\.openweathermap\.org\/data\//.test(evt.request.url)) {
+        // Cache weather responses to EXT_CACHE.
         // Try network and if it fails, go for the cached copy.
-        const res = fromNetwork(evt.request, 3e3).catch(() => fromCache(evt.request));
+        const res = fromNetwork(evt.request, 3e3, EXT_CACHE).catch(() => fromCache(evt.request, EXT_CACHE));
         if (DEBUG) console.log(evt.request.url, 'network-first response', res);
         evt.respondWith(res);
     } else {
+        // Cache typekit responses to EXT_CACHE and app sources to APP_CACHE.
         // Try cache and if it fails, go for the network copy.
-        const res = fromCache(evt.request).catch(() => fromNetwork(evt.request, 180e3));
+        const cacheName = /^https:\/\/[^\.]*\.?typekit\.net\//.test(evt.request.url) ? EXT_CACHE : APP_CACHE;
+        const res = fromCache(evt.request, cacheName).catch(() => fromNetwork(evt.request, 180e3, cacheName));
         if (DEBUG) console.log(evt.request.url, "read-through cache response", res);
         evt.respondWith(res);
     }
@@ -40,8 +44,8 @@ self.addEventListener('fetch', async (evt) => {
 
 // Time limited network request. If the network fails or the response is not
 // served before timeout, the promise is rejected.
-function fromNetwork(request, timeout, addToCache = true) {
-    if (DEBUG) console.log("fromNetwork", request.url);
+function fromNetwork(request, timeout, cacheName, addToCache = true) {
+    if (DEBUG) console.log("fromNetwork", request.url, cacheName);
     return new Promise(function (fulfill, reject) {
         // Reject in case of timeout.
         var timeoutId = setTimeout(reject, timeout);
@@ -49,7 +53,7 @@ function fromNetwork(request, timeout, addToCache = true) {
         fetch(request.clone()).then(function (response) {
             clearTimeout(timeoutId);
             if (addToCache && response.status < 400 && response.status !== 206) {
-                caches.open(CACHE).then((cache) => cache.put(request.clone(), response.clone()));
+                caches.open(cacheName).then((cache) => cache.put(request.clone(), response.clone()));
             }
             fulfill(response.clone());
             // Reject also if network fetch rejects.
@@ -60,9 +64,9 @@ function fromNetwork(request, timeout, addToCache = true) {
 // Open the cache where the assets were stored and search for the requested
 // resource. Notice that in case of no matching, the promise still resolves
 // but it does with `undefined` as value.
-function fromCache(request) {
-    if (DEBUG) console.log("fromCache", request.url);
-    return caches.open(CACHE).then(function (cache) {
+function fromCache(request, cacheName) {
+    if (DEBUG) console.log("fromCache", request.url, cacheName);
+    return caches.open(cacheName).then(function (cache) {
         return cache.match(request.clone()).then(function (matching) {
             if (DEBUG) console.log("fromCache got", request.url, matching);
             return matching || Promise.reject('no-match');

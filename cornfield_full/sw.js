08f7e9b0330cdcd7aabb1a1f9ec3c15f1bdd8004
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-01-10 22:41:46 +0800

    Freelance booking ad

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v74';
+const APP_CACHE = 'cornfield-cache-v75';
 const LIB_CACHE = 'cornfield-lib-cache-v5';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

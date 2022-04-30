058a75ff84ef371083de110064fc3386319b3989
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-04-30 09:52:26 +0800

    weather version up

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v75';
+const APP_CACHE = 'cornfield-cache-v76';
 const LIB_CACHE = 'cornfield-lib-cache-v5';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

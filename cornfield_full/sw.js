472943de2e8281f9356b94968e80b200f4091f98
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-12-20 07:45:00 +0800

    fix clock opacity after hiding

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v90';
+const APP_CACHE = 'cornfield-cache-v91';
 const LIB_CACHE = 'cornfield-lib-cache-v5';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

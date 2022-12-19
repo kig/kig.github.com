73043abe5c746b56dccc15a8cdf828aee808cc8f
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-12-20 04:56:05 +0800

    use navigator.language for clock & date formatting

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v83';
+const APP_CACHE = 'cornfield-cache-v84';
 const LIB_CACHE = 'cornfield-lib-cache-v5';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

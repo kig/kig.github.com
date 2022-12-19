44f306cd182a3521c73aa2e22c39a6b89c84bdb4
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-12-20 05:03:56 +0800

    match date font size to the rest

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v86';
+const APP_CACHE = 'cornfield-cache-v87';
 const LIB_CACHE = 'cornfield-lib-cache-v5';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

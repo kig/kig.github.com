267cb92e1d6917b430c4ed51c57586313a555f71
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-12-20 05:02:48 +0800

    aligh fullscreen icon with weather data

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v85';
+const APP_CACHE = 'cornfield-cache-v86';
 const LIB_CACHE = 'cornfield-lib-cache-v5';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

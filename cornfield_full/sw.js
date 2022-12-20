7e417764e33e452f0145f3f45aff12fd64695ccf
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-12-21 07:48:33 +0800

    add transition delay to hr

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v109';
+const APP_CACHE = 'cornfield-cache-v110';
 const LIB_CACHE = 'cornfield-lib-cache-v5';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

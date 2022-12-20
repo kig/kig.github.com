0600533a6f0af946990c556e494b62403723b61c
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-12-20 14:59:54 +0800

    faster hr fadeout

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v105';
+const APP_CACHE = 'cornfield-cache-v106';
 const LIB_CACHE = 'cornfield-lib-cache-v5';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

b31bf43b096379ac85dc011a555d524719708b24
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2023-02-09 21:53:48 +0800

    fix narrow layout for sunrise and sunset

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v134';
+const APP_CACHE = 'cornfield-cache-v135';
 const LIB_CACHE = 'cornfield-lib-cache-v5';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

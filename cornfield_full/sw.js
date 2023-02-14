ec4a521bff95bf7b1c9a6bcb7a1758ec1549def4
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2023-02-14 18:11:33 +0800

    night time city list icon should be white

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v144';
+const APP_CACHE = 'cornfield-cache-v145';
 const LIB_CACHE = 'cornfield-lib-cache-v6';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

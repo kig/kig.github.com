5224337b56981cb30c6d75be4b3f6d8896ae59e6
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2023-02-16 12:56:01 +0800

    remove touch preventDefault

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v149';
+const APP_CACHE = 'cornfield-cache-v150';
 const LIB_CACHE = 'cornfield-lib-cache-v6';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

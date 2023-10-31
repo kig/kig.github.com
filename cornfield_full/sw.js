fb2b33aba7b9f555844fa9e7bd5b11e67d63604d
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2023-10-31 12:52:00 +0800

    fix for missing aqi data

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v220';
+const APP_CACHE = 'cornfield-cache-v221';
 const LIB_CACHE = 'cornfield-lib-cache-v24';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

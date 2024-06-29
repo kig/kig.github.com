3187e7bc80e634a2015651c8d3058f48392bd8b4
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2024-06-29 20:38:50 +0800

    fix fades, perhaps.

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v222';
+const APP_CACHE = 'cornfield-cache-v225';
 const LIB_CACHE = 'cornfield-lib-cache-v26';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;

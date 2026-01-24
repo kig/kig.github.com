9f5491a21aea58779a7af7c1ec1c0c88aa326528
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2026-01-24 10:33:20 +0800

    Add left margin to city list

--- cornfield_full/sw.js
+++ cornfield_full/sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE = 'cornfield-cache-v251';
+const APP_CACHE = 'cornfield-cache-v252';
 const LIB_CACHE = 'cornfield-lib-cache-v27';
 const EXT_CACHE = 'cornfield-ext-cache';
 const DEBUG = false;
